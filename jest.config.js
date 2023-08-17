86d10335626ea9f8acd2c288ec8e3b7a27eca4fc
Bilal <bilaldjedovic@gmail.com>
2023-08-17 23:57:28 +0200

    changes

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-const { webdriverVersion } = require("./webdriver.config");
 
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   runner: "jest-serial-runner",
-  globals: {
-    webdriverVersion: webdriverVersion,
-  },
 };
